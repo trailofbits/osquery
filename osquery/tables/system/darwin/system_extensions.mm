cab5b10ff8657a237f3db80c5b3198f80e0557bd
AkshayK <akshay.kumar@octolabs.io>
2021-01-22 11:44:57 -0500

    Add is_managed flag with system extension table

--- osquery/tables/system/darwin/system_extensions.mm
+++ osquery/tables/system/darwin/system_extensions.mm
@@ -48,6 +48,58 @@ void getExtensionRow(const pt::ptree& extension, Row& r) {
   const auto category = extension.get_child("categories");
   r["category"] = getExtensionCategory(category);
   r["bundle_path"] = extension.get("container.bundlePath", "");
+  r["is_managed"] = INTEGER(0);
+}
+
+// com.apple.system-extension-policy payload defines the policy for
+// the system extensions. The function looks for team identifier or
+// extension bundle identifiers in the allowed list.
+// https://developer.apple.com/documentation/devicemanagement/systemextensions?language=objc
+void setManagedFlag(const pt::ptree& ptree, Row& r) {
+  const auto policies_attr_opt = ptree.get_child_optional("extensionPolicies");
+  if (!policies_attr_opt.has_value()) {
+    return;
+  }
+
+  const auto& policies_attr = policies_attr_opt.value();
+  const auto row_teamid = r["team"];
+  const auto row_identifier = r["identifier"];
+
+  for (const auto& policy : policies_attr) {
+    // Get the list of allowed team identifiers and check if the extension
+    // is managed
+    const auto& policy_item = policy.second;
+    const auto allowed_team_opt =
+        policy_item.get_child_optional("allowedTeamIDs");
+    if (allowed_team_opt.has_value()) {
+      const auto& allowed_team = allowed_team_opt.value();
+      for (const auto& team : allowed_team) {
+        const auto teamid = team.second.get("", "");
+        if (teamid == row_teamid) {
+          r["is_managed"] = INTEGER(1);
+        }
+      }
+    }
+
+    // get the list of allowed extensions and check if the bundle identifier is
+    // managed
+    const auto allowed_extensions_opt =
+        policy_item.get_child_optional("allowedExtensions");
+    if (allowed_extensions_opt.has_value()) {
+      const auto& allowed_extensions = allowed_extensions_opt.value();
+      const auto extensions_opt =
+          allowed_extensions.get_child_optional(row_teamid);
+      if (extensions_opt.has_value()) {
+        const auto& extensions = extensions_opt.value();
+        for (const auto& extension : extensions) {
+          auto identifier = extension.second.get("", "");
+          if (identifier == row_identifier) {
+            r["is_managed"] = INTEGER(1);
+          }
+        }
+      }
+    }
+  }
 }
 
 QueryData genExtensionsFromPtree(const pt::ptree& ptree) {
@@ -62,6 +114,7 @@ QueryData genExtensionsFromPtree(const pt::ptree& ptree) {
     const auto& extension_value = array_entry.second;
     Row r;
     getExtensionRow(extension_value, r);
+    setManagedFlag(ptree, r);
     results.push_back(r);
   }
   return results;
