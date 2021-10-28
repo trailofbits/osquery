24730d1f8e8a39d2e82c3d57d48c496c34a17156
Adam Meily <adam.meily@trailofbits.com>
2021-10-28 13:22:34 -0400

    add TODO note about arm64e

--- osquery/tables/system/darwin/signature.mm
+++ osquery/tables/system/darwin/signature.mm
@@ -29,6 +29,8 @@ namespace osquery {
 namespace tables {
 
 // Empty string runs default verification on a file
+// TODO: we may want to eventually add arm64e to this set. As of October 2021,
+// arm64 and arm64e are aliased and duplicated results are returned.
 std::set<std::string> kCheckedArches{
     "", "i386", "ppc", "arm", "x86_64", "arm64"};
 
